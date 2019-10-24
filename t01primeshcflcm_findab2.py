e78bd07904638fdcacf79a3f1ce653cd0005f529
jolenekueh1e <53029733+jolenekueh1e@users.noreply.github.com>
2019-10-24 15:09:01 +0800

    solved t01primeshcflcm_findab2.py

--- t01primeshcflcm_findab2.py
+++ t01primeshcflcm_findab2.py
@@ -1,2 +1,7 @@
 # If a and b are integers such that a * b = 18, find the possible values of a + b.
 
+for a in range (1,13):
+  for b in range (1,13):
+    if a * b == 13:
+      print (a + b)
+      
